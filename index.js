bf324bb4b35f7549364a91517e117621865070ae
Himanshu Tiwari <htiwari578@gmail.com>
2025-10-26 23:59:35 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import movieRouter from "./routes/movie.route.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +10,16 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+// test route
+app.get("/test", (req, res) => {
+  console.log("✅ /test route hit");
+  res.json({ message: "Server is working fine!" });
+});
+
+//apis
+app.use("/api/movies", movieRouter);
 
 const PORT = process.env.PORT || 3000;
 
