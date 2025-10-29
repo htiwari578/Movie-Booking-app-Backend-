4aa5889eb40457fb70cf330072115981a4db5a4c
Himanshu Tiwari <htiwari578@gmail.com>
2025-10-30 00:18:49 +0530

    updated code

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -19,3 +19,36 @@ export const createMovie = async (req, res) => {
         });
     }
 }
+
+export const deleteMovie = async (req, res) => {
+    try {
+        const response = await Movie.deleteOne({_id: req.params.id});
+        return res.status(200).json({
+            success: true,
+            message: "Movie deleted successfully",
+            data: response
+        });
+    } catch (error) {
+        console.error("Error deleting movie:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+export const getMovie = async(req,res)=>{
+    try {
+        const movie=await Movie.findById(req.params.id);
+        return res.status(200).json({
+            success:true,
+            message:"Movie fetched successfully",
+            data:movie
+        });
+    } catch (error) {
+        console.error("Error fetching movie:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
