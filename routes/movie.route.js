4ac740e481c71413f24fbaf384bfdc017ca478e2
Himanshu Tiwari <htiwari578@gmail.com>
2025-10-30 00:22:14 +0530

    updates routes

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createMovie } from "../controllers/movie.controller.js";
+import { createMovie, deleteMovie } from "../controllers/movie.controller.js";
 
 const movieRouter= express.Router();
 
 
 movieRouter.post("/create",createMovie);
+movieRouter.delete("/deleteMovie",deleteMovie);
    
 
 export default movieRouter;
