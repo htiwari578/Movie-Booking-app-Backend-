47c103a48801753be83852c09217e80694353f41
Himanshu Tiwari <htiwari578@gmail.com>
2025-11-03 15:34:18 +0530

    updated code

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createMovie, deleteMovie } from "../controllers/movie.controller.js";
+import { createMovie, deleteMovie, getMovie } from "../controllers/movie.controller.js";
 
 const movieRouter= express.Router();
 
 
 movieRouter.post("/create",createMovie);
 movieRouter.delete("/deleteMovie",deleteMovie);
-   
+movieRouter.get("/getMovie/:id",getMovie);
 
 export default movieRouter;
 
