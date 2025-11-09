fa3cb1aa76abc5589271e833e09cc5af20bc27e3
Himanshu Tiwari <htiwari578@gmail.com>
2025-11-09 23:21:41 +0530

    updated code

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,11 +1,16 @@
 import Movie from "../models/movie.model.js";
 import movieService from "../services/movie.service.js";
 
-const responseBody = {
+const errorResponseBody = {
     err: {},
     data: {},
     message: "Something went wrong,unable to fetch movie",
     success: false
+}
+const successResponseBody = {
+    data: {},
+    message: "Movie fetched successfully",
+    success: true
 }
 
 export const createMovie = async (req, res) => {
@@ -48,14 +53,11 @@ export const getMovie = async(req,res)=>{
     try {
         const response = await movieService.getMovieById(req.params.id);
         if(response.err){
-            responseBody.err = response.err;
-            return res.status(response.code).json(responseBody);
+            errorResponseBody.err = response.err;
+            return res.status(response.code).json(errorResponseBody);
         }
-        return res.status(200).json({
-            success:true,
-            message:"Movie fetched successfully",
-            data:response,
-        });
+        successResponseBody.data = response;
+        return res.status(200).json(successResponseBody);
     } catch (error) {
         console.error("Error fetching movie:", error);
         return res.status(500).json({
